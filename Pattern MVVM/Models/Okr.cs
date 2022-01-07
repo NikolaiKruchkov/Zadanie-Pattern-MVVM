4b28e812902bf38d7432a42e07177ac423769c7d
NikolaiKruchkov <47074017+NikolaiKruchkov@users.noreply.github.com>
2022-01-07 16:34:53 +0300

    Add files via upload

---
+++ Pattern MVVM/Models/Okr.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pattern_MVVM.Models
+{
+    static class Okr
+    {
+        public static double RaschetDlina(double a) => 2 * Math.PI * a;
+        public static double RaschetPloshad(double a) => Math.PI * Math.Pow(a, 2);
+    }
+}
---
+++ Pattern MVVM/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,76 @@
+﻿using Pattern_MVVM.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Pattern_MVVM.ViewModels
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        void OnPropertyChanged([CallerMemberName]string PropertyName=null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+        private double radius;
+        public double Radius
+        {
+            get => radius;
+            set
+            {
+                radius = value;
+                OnPropertyChanged();
+            }
+        }
+        private double dlina;
+        public double Dlina
+        {
+            get => dlina;
+            set
+            {
+                dlina = value;
+                OnPropertyChanged();
+            }
+        }
+        private double ploshad;
+        public double Ploshad
+        {
+            get => ploshad;
+            set
+            {
+                ploshad = value;
+                OnPropertyChanged();
+            }
+        }
+        public ICommand PoschitaemDlinu { get; }
+        private void OnPoschitaemDlinuExecute(object p)
+        {
+            Dlina = Okr.RaschetDlina(Radius);
+            
+        }
+        private bool CanPoschitaemDlinuIPloshadExecuted(object p)
+        {
+            if (Radius != 0)
+                return true;
+            else
+                return false;
+        }
+        public ICommand PoschitaemPloshad { get; }
+        private void OnPoschitaemPloshadExecute(object p)
+        {
+            
+            Ploshad = Okr.RaschetPloshad(Radius);
+        }
+        
+        public MainWindowViewModel()
+        {
+            PoschitaemDlinu = new RelayCommand(OnPoschitaemDlinuExecute, CanPoschitaemDlinuIPloshadExecuted);
+            PoschitaemPloshad = new RelayCommand(OnPoschitaemPloshadExecute, CanPoschitaemDlinuIPloshadExecuted);
+        }
+    }
+}
